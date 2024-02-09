0ee853ad0c69554fa3ef2b6dc4475f4d8b596139
kizuyoko <kizuyoko@gmail.com>
2024-02-09 17:44:21 +0100

    Project Start

--- app/page.js
+++ app/page.js
@@ -1,12 +1,9 @@
-import Link from "next/link";
-import Header from "./components/header";
-
 export default function Home() {
   return (
     <main>
-      <Header />
-      <p><Link href='/about'>About</Link></p>
-      <p><Link href='/blog'>Blog</Link></p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
