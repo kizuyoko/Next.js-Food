bbd0b68906d5b88fd3c5d6fc79416ec69966ae50
kizuyoko <kizuyoko@gmail.com>
2024-02-12 11:20:04 +0100

    Organizing components and files

---
+++ app/components/main-header/main-header.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import logoImg from '@/assets/logo.png';
+import classes from './main-header.module.css'
+import Image from "next/image";
+import MainHeaderBackground from "./main-header-background";
+// 'Image' optimizes the image, Add 'priority' to be prio.
+
+export default function MainHeader () {
+  return (<>
+    <MainHeaderBackground />
+    <header className={classes.header}>
+      <Link className={classes.logo} href='/'>
+        <Image src={logoImg} alt='Food' priority />
+        Next Level Food
+      </Link>
+
+      <nav className={classes.nav}>
+        <ul>
+          <li>
+            <Link href='/meals'>Meals</Link>
+          </li>
+          <li>
+            <Link href='/community'>Community</Link>
+          </li>
+        </ul>
+
+      </nav>
+    </header>
+  </>)
+}
