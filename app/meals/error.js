75a7dc0b1fffbfc043b0f845cedbada882eb063b
kizuyoko <kizuyoko@gmail.com>
2024-02-13 22:21:52 +0100

    Error

---
+++ app/meals/error.js
@@ -0,0 +1,8 @@
+'use client';
+
+export default function Error() {
+  return <main className="error">
+    <h1>An error occured!</h1>
+    <p>An error occured. Please try again later.</p>
+  </main>
+}
