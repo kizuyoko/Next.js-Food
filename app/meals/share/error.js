8ad13ec9485af3ebbdd9c333c81bc14bbd42f14c
kizuyoko <kizuyoko@gmail.com>
2024-03-06 11:35:48 +0100

    Validation with useFormState

---
+++ app/meals/share/error.js
@@ -0,0 +1,8 @@
+'use client';
+
+export default function Error() {
+  return <main className="error">
+    <h1>An error occured!</h1>
+    <p>Failed to create meal.</p>
+  </main>
+}
--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,9 +1,12 @@
+'use client';
 import ImagePicker from '@/app/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeal } from '@/lib/actions';
 import MealsFormSubmit from '@/app/components/meals/meals-form-submit';
+import { useFormState } from 'react-dom';
 
 export default function ShareMealPage() {
+  const [state, formAction] = useFormState(shareMeal, {message: null});
   return (
     <>
       <header className={classes.header}>
@@ -13,7 +16,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={shareMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -42,6 +45,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={classes.actions}>
             <MealsFormSubmit />
           </p>
